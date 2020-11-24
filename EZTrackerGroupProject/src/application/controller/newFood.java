e5ed8c0315772740272c4bb494bbdbfff6d953d9
User <AmberBeserra@gmail.com>
2020-11-23 19:30:13 -0600

    Changed textfield output to text area

--- EZTrackerGroupProject/src/application/controller/newFood.java
+++ EZTrackerGroupProject/src/application/controller/newFood.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.fxml.Initializable;
@@ -37,7 +38,7 @@ public class newFood
 	@FXML
 	private TextField protein;
 	@FXML
-	private TextField output;
+	private TextArea output;
 
 	static ArrayList<Food> meal = new ArrayList<Food>();
 
